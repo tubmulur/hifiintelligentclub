ea83c9be1167474ff5add4d71a562a949bdab0c9
Andrey Chekmarev <assminog@gmail.com>
2020-06-23 02:04:20 +0300

    Initial

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+require_once('/home/EDRO.SetOfTools/System/0.Functions/0.strNDigit.php');
+require_once('/home/EDRO.SetOfTools/System/0.Functions/1.RequestsFilter.php');
+require_once('/home/EDRO.SetOfTools/System/1.Reporter/0.ReportError.php');
+require_once('/home/EDRO.SetOfTools/System/1.Reporter/1.Report.php');
+require_once('/home/EDRO.SetOfTools/System/2.VectorKIIM/0.KIIM.php');
+require_once('/home/EDRO.SetOfTools/System/2.VectorKIIM/1.objKIIM.activation.php');
+require_once('/home/EDRO.SetOfTools/System/3.Buffer/0.EDRO_Loader.php');
+require_once('/home/EDRO.SetOfTools/System/3.Buffer/1.EDRO_Buffering.php');
+
+
+
+$objEDRO=new EDRO($objKIIM);
+
+
+require_once('/home/EDRO.SetOfTools/System/6.HTML_Interfaces/3.HTML_CloseInterface.php');
+?>
