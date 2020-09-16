eef1320e41793721fec581f2a97900d36140df96
Andrey Chekmarev <assminog@gmail.com>
2020-09-16 07:18:10 +0300

    VectorKIIM template added

--- index.php
+++ index.php
@@ -6,12 +6,13 @@ require_once('/home/EDRO.SetOfTools/System/1.Reporter/0.ReportError.php');
 require_once('/home/EDRO.SetOfTools/System/1.Reporter/1.Report.php');
 require_once('/home/EDRO.SetOfTools/System/2.VectorKIIM/0.KIIM.php');
 require_once('/home/EDRO.SetOfTools/System/2.VectorKIIM/1.objKIIM.activation.php');
-
+require_once('/home/EDRO.SetOfTools/System/5.Templates/0.strKIIM.Template.php');
+echo $strKIIMWindowHTML;
 require_once('/home/EDRO.SetOfTools/System/3.Buffer/1.EDRO_Buffering.php');
 
 $objEDRO	=new Event($objKIIM);
 FileRead::_GetDesignHTML($objKIIM, $objEDRO->arrDesign['strTemplate'], $objEDRO);
-//$objEDRO=new EDRO($objKIIM);
+
 
 
 require_once('/home/EDRO.SetOfTools/System/6.HTML_Interfaces/3.HTML_CloseInterface.php');
